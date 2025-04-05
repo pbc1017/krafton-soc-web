6a99a406560b0a06efe3f3f0deaec27ad683e3df
perry <perry@channel.io>
2025-04-06 02:51:03 +0900

    refactor: simplify Footer component layout by removing absolute positioning

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -9,109 +9,77 @@ import Text from '@krafton-soc/common/components/Text';
 const FooterContainer = styled.footer`
   background-color: ${theme.colors.white};
   height: 135px;
-  position: relative;
   width: 100%;
-  max-width: 1440px;
-  margin: 0 auto;
   display: flex;
-  align-items: center;
-  padding: 0 2rem;
+  justify-content: space-between;
+`;
 
-  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
-    height: auto;
-    padding: 2rem;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 1.5rem;
-  }
+const LeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 27px;
+  margin-left: 109px;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  gap: 60px;
+  cursor: pointer;
 `;
 
 const CopyrightContainer = styled.div`
-  position: absolute;
-  left: 118px;
-  top: 100px;
-  white-space: nowrap;
-
-  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
-    position: static;
-    margin-top: 1rem;
-  }
-`;
-
-const KaistLogoContainer = styled.div`
-  height: 21px;
-  width: 127px;
-  position: absolute;
-  left: 109px;
-  top: 27px;
-
-  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
-    position: static;
-    margin-bottom: 1rem;
-  }
-`;
-
-const KraftonLogoContainer = styled.div`
-  height: 18px;
-  width: 105px;
-  position: absolute;
-  left: 296px;
-  top: 26px;
-
-  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
-    position: static;
-    margin-bottom: 1rem;
-  }
+  margin-top: 53px;
+  margin-left: 9px;
 `;
 
 const ContactContainer = styled.div`
-  position: absolute;
-  left: 712px;
-  top: 26px;
-  width: 474px;
-
-  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
-    position: static;
-    width: 100%;
-  }
+  margin-top: 25px;
+  margin-right: 109px;
 `;
 
 export const Footer: React.FC = () => {
   return (
     <FooterContainer>
-      <KaistLogoContainer>
-        <Image
-          src="/images/common/soc-logo.svg"
-          alt="KAIST School of Computing"
-          width={127}
-          height={21}
-          priority
-          objectFit="contain"
-        />
-      </KaistLogoContainer>
-
-      <KraftonLogoContainer>
-        <Image
-          src="/images/common/krafton-logo.svg"
-          alt="KRAFTON"
-          width={105}
-          height={18}
-          priority
-          objectFit="contain"
-        />
-      </KraftonLogoContainer>
-
-      <CopyrightContainer>
-        <Text color={theme.colors.black} fs="9px" fw={theme.fonts.weights.regular} lh="11.7px">
-          ⓒ KAIST School of Computing. All Rights Reserved.
-        </Text>
-      </CopyrightContainer>
+      <LeftSection>
+        <LogoContainer>
+          <Image
+            src="/images/common/soc-logo.svg"
+            alt="KAIST School of Computing"
+            width={127}
+            height={21}
+            priority
+            objectFit="contain"
+            onClick={() => {
+              window.open('https://cs.kaist.ac.kr/', '_blank');
+            }}
+          />
+          <Image
+            src="/images/common/krafton-logo.svg"
+            alt="KRAFTON"
+            width={105}
+            height={18}
+            priority
+            objectFit="contain"
+            onClick={() => {
+              window.open('https://www.krafton.com/', '_blank');
+            }}
+          />
+        </LogoContainer>
+        <CopyrightContainer>
+          <Text fs="9px" fw={theme.fonts.weights.regular} lh="11.7px">
+            ⓒ KAIST School of Computing. All Rights Reserved.
+          </Text>
+        </CopyrightContainer>
+      </LeftSection>
 
       <ContactContainer>
-        <Text color={theme.colors.black} fs="11px" fw={theme.fonts.weights.semibold} lh="16.5px">
-          수정이 필요하거나 개선할 내용이 있으면 언제든지 아래 이메일로 문의해주시면 감사하겠습니다.
+        <Text fs="11px" fw={theme.fonts.weights.semibold} lh="16.5px">
+          보다 나은 서비스를 제공하기 위해 여러분의 의견을 소중히 생각합니다.
           <br />
-          kraftonxsoc@kaist.ac.kr
+          사이트 이용 중 불편한 점이나 수정이 필요한 내용이 있다면 언제든지 아래 이메일로 문의해
+          주세요.
+          <br />
+          CS@kraftonxsoc.com
         </Text>
       </ContactContainer>
     </FooterContainer>
