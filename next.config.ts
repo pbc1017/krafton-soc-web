c1762b61b6ccde175d5b5a8a24bcc41970e9f331
perry <perry@channel.io>
2025-04-08 06:03:01 +0900

    chore: remove default locale comments from next.config.ts and add a blank line in next-rule.mdc for clarity

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin(
-  // This is the default locale you want to use in your app.
-  // The middleware will redirect users navigating to `/` to `/en`
-  "./src/i18n/request.ts",
-);
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
