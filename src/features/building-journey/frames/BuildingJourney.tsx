6820489fb51e6e912d9dd04c736f856c8de03bbc
Gerbera3090 <hw3090@gmail.com>
2025-04-21 15:28:10 +0900

    chore: add files for structure

---
+++ src/features/building-journey/frames/BuildingJourney.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Text from "@krafton-soc/common/components/Text";
+
+const BuildingJourney: React.FC = () => {
+  return <Text>Building Journey</Text>;
+};
+
+export default BuildingJourney;
