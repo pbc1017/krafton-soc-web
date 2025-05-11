a38d3c3a5ebe1de1024b174eb757d164e3c51c65
Gerbera3090 <hw3090@gmail.com>
2025-05-12 07:17:22 +0900

    feat: positive impace banner

--- src/features/positive-impact/components/PositiveImpactBanner.tsx
+++ src/features/positive-impact/components/PositiveImpactBanner.tsx
@@ -1,5 +1,94 @@
-const PositiveImpactBanner = () => {
-  return <div>PositiveImpactBanner</div>;
-};
+"use client";
 
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+import Text from "@krafton-soc/common/components/Text";
+import { theme } from "@krafton-soc/common/styles/theme";
+
+const BannerContainer = styled.section`
+  margin-top: 72px;
+  width: 100%;
+  height: 626px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: -1;
+  position: relative;
+`;
+
+const BannerImage = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
+
+const BannerTextContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+  height: 93px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 2;
+`;
+
+const BannerTitle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+  font-size: 32px;
+  line-height: 110%;
+  font-weight: ${theme.fonts.weights.medium};
+  color: ${theme.colors.white};
+`;
+
+const BannerText = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+  font-size: 18px;
+  line-height: 110%;
+  font-weight: ${theme.fonts.weights.regular};
+  color: ${theme.colors.white};
+`;
+
+const PositiveImpactBanner: React.FC = () => {
+  return (
+    <BannerContainer>
+      <BannerImage>
+        <Image
+          src="/images/positive-impact/banner.png"
+          alt="Positive Impact Banner"
+          fill
+          style={{ objectFit: "cover" }}
+          quality={100}
+        />
+      </BannerImage>
+      <BannerTextContainer>
+        <BannerTitle>
+          <Text>{"Where Positive Influence Comes Full Circle"}</Text>
+        </BannerTitle>
+        <BannerText>
+          <Text>{"선한 영향력이 선순환하는 공간"}</Text>
+        </BannerText>
+      </BannerTextContainer>
+    </BannerContainer>
+  );
+};
 export default PositiveImpactBanner;
