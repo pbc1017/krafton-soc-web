d0d8bb734269585862066b98a1375bf354502d2f
Gerbera3090 <hw3090@gmail.com>
2025-05-14 09:54:40 +0900

    feat: add translate and responsive for design story banner

--- src/features/design-story/components/DesignStoryBanner.tsx
+++ src/features/design-story/components/DesignStoryBanner.tsx
@@ -2,10 +2,12 @@
 
 import styled from "@emotion/styled";
 import Image from "next/image";
+import { useLocale, useTranslations } from "next-intl";
 
 import Text from "@krafton-soc/common/components/Text";
 import { useResponsiveStore } from "@krafton-soc/common/stores/useResponsiveStore";
 import { theme } from "@krafton-soc/common/styles/theme";
+import { Locale } from "@krafton-soc/i18n/config";
 
 const BannerContainer = styled.section`
   width: 100%;
@@ -31,17 +33,52 @@ const BannerTextContainer = styled.div`
   top: 50%;
   transform: translateY(-50%);
   width: 100%;
-  height: 93px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   z-index: 1;
 `;
 
+const BannerTitleText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+  font-size: 32px;
+  font-weight: ${theme.fonts.weights.medium};
+  line-height: 110%;
+  color: ${theme.colors.white};
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    font-size: 28px;
+  }
+`;
+
+const BannerSubText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+  font-size: 24px;
+  font-weight: ${theme.fonts.weights.regular};
+  line-height: 110%;
+  color: ${theme.colors.white};
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    font-size: 22.2px;
+  }
+`;
+
 const DesignStoryBanner: React.FC = () => {
-  const { windowWidth } = useResponsiveStore();
-  console.log(windowWidth);
+  const locale = useLocale() as Locale;
+  const { deviceType } = useResponsiveStore();
+  const t = useTranslations("DesignStory.banner");
+
   return (
     <BannerContainer>
       <BannerImage>
@@ -54,22 +91,19 @@ const DesignStoryBanner: React.FC = () => {
         />
       </BannerImage>
       <BannerTextContainer>
-        <Text
-          fs="32px"
-          fw={theme.fonts.weights.medium}
-          color={theme.colors.white}
-          lh="110%"
-        >
-          KRAFTON X SoC, Design Story
-        </Text>
-        <Text
-          fs="24px"
-          fw={theme.fonts.weights.regular}
-          color={theme.colors.white}
-          lh="110%"
-        >
-          KRAFTON X SoC, 디자인 이야기
-        </Text>
+        <BannerTitleText>
+          <Text>{t(`title.${deviceType}`)}</Text>
+        </BannerTitleText>
+        {locale === "ko" && (
+          <>
+            <BannerTitleText>
+              <Text>{"\n"}</Text>
+            </BannerTitleText>
+            <BannerSubText>
+              <Text>{t(`subTitle.${deviceType}`)}</Text>
+            </BannerSubText>
+          </>
+        )}
       </BannerTextContainer>
     </BannerContainer>
   );
--- src/features/design-story/components/DesignStoryIntro/DesignStoryIntroCard1.tsx
+++ src/features/design-story/components/DesignStoryIntro/DesignStoryIntroCard1.tsx
@@ -12,14 +12,69 @@ const IntroStartContainer = styled.section`
   flex-direction: row;
   align-items: flex-start;
   justify-content: space-between;
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 24px;
+    gap: 110px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 0px;
+    gap: 60px;
+  }
+
   //background-color: pink;
 `;
 
 const ImageContainer = styled.section`
   position: relative;
   width: 740px;
-  //background-color: orange;
   gap: 7px;
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    width: 577px;
+    gap: 10px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    width: 373px;
+    gap: 10px;
+  }
+  //background-color: orange;
+`;
+
+const CardImage = styled.div`
+  position: relative;
+  width: 100%;
+
+  height: 600px;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    height: 468px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    height: 303px;
+  }
+`;
+
+const CardImageCaption = styled.div`
+  width: 100%;
+  font-size: 14px;
+  font-weight: ${theme.fonts.weights.regular};
+  line-height: 120%;
+  color: ${theme.colors.black};
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    font-size: 12px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    margin-left: 20px;
+  }
 `;
 
 const TextContainer = styled.section`
@@ -28,45 +83,62 @@ const TextContainer = styled.section`
   flex-direction: column;
   justify-content: flex-start;
   gap: 45px;
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    margin-left: 20px;
+  }
   //background-color: yellow;
+`;
+
+const TextTitle = styled.div`
+  font-size: 64px;
+  font-weight: ${theme.fonts.weights.semibold};
+  line-height: 130%;
+  color: ${theme.colors.black};
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    font-size: 48px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    font-size: 32px;
+  }
+`;
+
+const TextContent = styled.div`
+  font-size: 18px;
+  font-weight: ${theme.fonts.weights.regular};
+  line-height: 180%;
+  color: ${theme.colors.black};
 `;
 
 const DesignStoryIntroStart = () => {
   return (
     <IntroStartContainer>
       <ImageContainer>
-        <Image
-          src="/images/DesignStory/Intro/card1.png"
-          alt="Design Story Intro Card 1"
-          width={740}
-          height={600}
-        />
-        <Text
-          fs="14px"
-          fw={theme.fonts.weights.regular}
-          color={theme.colors.black}
-          lh="120%"
-        >
-          1F Concrete Wall & Table
-        </Text>
+        <CardImage>
+          <Image
+            src="/images/DesignStory/Intro/card1.png"
+            alt="Design Story Intro Card 1"
+            fill
+          />
+        </CardImage>
+        <CardImageCaption>
+          <Text>1F Concrete Wall & Table</Text>
+        </CardImageCaption>
       </ImageContainer>
       <TextContainer>
-        <Text
-          fs="64px"
-          fw={theme.fonts.weights.semibold}
-          color={theme.colors.black}
-          lh="130%"
-        >
-          {"우리의 시작을\n돌아보다"}
-        </Text>
-        <Text
-          fs="18px"
-          fw={theme.fonts.weights.regular}
-          color={theme.colors.black}
-          lh="180%"
-        >
-          {`처음 논의가 시작될 때, 우리가 가장 중요하게 던진 질문은\n"이 공간이 사용자들에게 어떤 의미를 가질 것인가?"였습니다.\n단순히 하나의 결과물로서 완성되는 것이 아닌,\n보이지 않는 선한 영향력을 공간을 통해 가시화하고자 했습니다.\n\n이 공간은 거대한 연결의 흐름 속에서 눈에 보이는 하나의 \n연결점이자 이정표와 같은 역할을 합니다. 그렇기에 이곳이 하나의 \n끝이 아니라, 새로운 연결이 시작되는 장소가 되기를 바랍니다.\n\n이 공간을 경험하는 사람들이 거대한 고리로 연결되어, 어떤 \n변화를 만들어 가는지, 그리고 자신은 그 흐름 속에서 어떤 \n연결을 만들어갈 것인지 자연스럽게 느낄 수 있기를 기대합니다.`}
-        </Text>
+        <TextTitle>
+          <Text>{"우리의 시작을\n돌아보다"}</Text>
+        </TextTitle>
+        <TextContent>
+          <Text>
+            {`처음 논의가 시작될 때, 우리가 가장 중요하게 던진 질문은\n"이 공간이 사용자들에게 어떤 의미를 가질 것인가?"였습니다.\n단순히 하나의 결과물로서 완성되는 것이 아닌,\n보이지 않는 선한 영향력을 공간을 통해 가시화하고자 했습니다.\n\n이 공간은 거대한 연결의 흐름 속에서 눈에 보이는 하나의 \n연결점이자 이정표와 같은 역할을 합니다. 그렇기에 이곳이 하나의 \n끝이 아니라, 새로운 연결이 시작되는 장소가 되기를 바랍니다.\n\n이 공간을 경험하는 사람들이 거대한 고리로 연결되어, 어떤 \n변화를 만들어 가는지, 그리고 자신은 그 흐름 속에서 어떤 \n연결을 만들어갈 것인지 자연스럽게 느낄 수 있기를 기대합니다.`}
+          </Text>
+        </TextContent>
       </TextContainer>
     </IntroStartContainer>
   );
