79dcd95fef8a814938affcf0e21ea1ce46ee2656
perry <perry@channel.io>
2025-05-16 12:25:16 +0900

    style: improve layout and responsiveness of DesignStoryConceptCard3 with dynamic translations

--- src/features/DesignStory/components/DesignStoryConcept/DesignStoryConceptCard3.tsx
+++ src/features/DesignStory/components/DesignStoryConcept/DesignStoryConceptCard3.tsx
@@ -2,38 +2,57 @@
 
 import styled from "@emotion/styled";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 import Text from "@krafton-soc/common/components/Text";
+import { useResponsiveStore } from "@krafton-soc/common/stores/useResponsiveStore";
 import { theme } from "@krafton-soc/common/styles/theme";
 
+const MIN_GAP_PC = "24px";
+
 const CardContainer = styled.section`
-  width: 1285px;
+  width: 100%;
+  max-width: 1285px;
   display: flex;
   flex-direction: row;
-  align-items: flex-start;
+  align-items: stretch;
   justify-content: space-between;
+  margin: 250px auto 0;
 
-  //background-color: red;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    margin-top: 160px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 90px;
+  }
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    margin-top: 100px;
+    gap: 80px;
+  }
 `;
 
 const CardTextContainer = styled.section`
   width: 399px;
+  flex-shrink: 0;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  justify-content: space-between;
 
-  gap: 160px;
-
-  //background-color: green;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    width: 457px;
+    align-items: flex-start;
+    text-align: left;
+    gap: 20px;
+    justify-content: flex-start;
+  }
 `;
 
 const CardTextTitle = styled.section`
   width: 100%;
-
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-
   text-align: left;
   font-family: ${theme.fonts.families.pretendard};
   font-weight: ${theme.fonts.weights.medium};
@@ -41,7 +60,9 @@ const CardTextTitle = styled.section`
   line-height: 120%;
   color: ${theme.colors.black};
 
-  //background-color: yellow;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    font-size: 32px;
+  }
 `;
 
 const CardTextContent = styled.section`
@@ -52,61 +73,89 @@ const CardTextContent = styled.section`
   line-height: 180%;
   color: ${theme.colors.black};
 
-  //background-color: orange;
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+    font-size: 16px;
+  }
 `;
 
 const CardImageContainer = styled.section`
-  width: 764px;
+  flex-grow: 1;
+  flex-shrink: 1;
+  min-width: 300px;
+  max-width: 764px;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-
   gap: 10px;
 
-  //background-color: purple;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    width: 100%;
+    max-width: 100%;
+    align-items: flex-start;
+    flex-grow: 0;
+    flex-shrink: 0;
+  }
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+  }
 `;
 
 const CardImage = styled.div`
   width: 100%;
-  height: 573px;
+  height: auto;
+  aspect-ratio: 764 / 573;
   position: relative;
-
-  //background-color: pink;
 `;
 
 const CardImageCaption = styled.section`
   width: 100%;
-
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-
   text-align: right;
-
   font-family: ${theme.fonts.families.pretendard};
   font-weight: ${theme.fonts.weights.regular};
   font-size: 14px;
   line-height: 120%;
   color: ${theme.colors.black};
 
-  //background-color: wheat;
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    font-size: 12px;
+    align-items: flex-start;
+    text-align: left;
+  }
+  @media (max-width: ${theme.breakpoints.mobileTablet}) {
+  }
+`;
+
+const Spacer = styled.div`
+  flex-shrink: 0;
+  width: ${MIN_GAP_PC};
+
+  @media (max-width: ${theme.breakpoints.tabletDesktop}) {
+    width: 0;
+    height: 0;
+  }
 `;
 
 const DesignStoryConceptCard3 = () => {
+  const { deviceType } = useResponsiveStore();
+  const t = useTranslations("DesignStory.Concept.Card3");
+
   return (
     <CardContainer>
       <CardTextContainer>
         <CardTextTitle>
-          <Text>{`Heritage, \nNot a Narrative`}</Text>
+          <Text>{t(`title.${deviceType}`)}</Text>
         </CardTextTitle>
         <CardTextContent>
-          <Text>{`‘Heritage, Not a Narrative’는 이번 프로젝트를 \n관통하는 가장 중요한 개념이었습니다. \n기부자들이 자신의 존재를 부각시키기보다, 후배들에게 \n자연스럽게 유산을 남기고자 하는 의지를 담아 \n공간 속에서 은은하게 드러나도록 디자인했습니다.\n\n함께 성장하는 ‘Reflective Jungle’의 식물 사이로 슈퍼미러에 새겨진 기부자의 메시지는 단순한 일방적인 기념물이 아니라, 사용자의 움직임에 따라 변화하고 \n투영되는 요소로 작용합니다.`}</Text>
+          <Text>{t(`content.${deviceType}`)}</Text>
         </CardTextContent>
       </CardTextContainer>
+      <Spacer />
       <CardImageContainer>
         <CardImage>
           <Image
-            src={"/images/DesignStory/Concept/card3.png"}
+            src={`/images/DesignStory/Concept/디자인이야기_Con5_${deviceType}.png`}
             alt="design-story-concept-card3"
             fill
             style={{ objectFit: "cover" }}
